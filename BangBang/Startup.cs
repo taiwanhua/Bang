71ebc180fde5bc0c7b31e8a04167a49f17aa95da
Arhua <a0987837233@gmail.com>
2019-09-06 13:14:03 +0800

    issue00001 初始化

---
+++ BangBang/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BangBang.Startup))]
+namespace BangBang
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
