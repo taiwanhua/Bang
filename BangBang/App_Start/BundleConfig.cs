bbb92cee76bad0f8474c235e3788f0ee45a12cfe
Arhua <a0987837233@gmail.com>
2019-09-07 14:32:30 +0800

    增加指定配置LocalDB，而非MSSQLEXPRESS，能在專案內附帶mdf檔

--- BangBang/App_Start/BundleConfig.cs
+++ BangBang/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace BangBang
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/min.css"));
         }
     }
 }
--- BangBang/Models/DataDbContext.cs
+++ BangBang/Models/DataDbContext.cs
@@ -7,8 +7,13 @@ using System.Web;
 namespace BangBang.Models
 {
 
-    public class DataDbContext:DbContext
+    public class DataDbContext : DbContext
     {
+        //將使用LocalDB，因為感覺使用SQLserverExpress，放到GIThub上再下載到另一部機器會連不上
+        public DataDbContext() : base("DefaultConnection")
+        {
+
+        }
         public DbSet<Player> Players { get; set; }
         public DbSet<Walletrecord> Walletrecords { get; set; }
         public DbSet<GameRecord> GameRecords { get; set; }
