04e790c0e7dad02089ba621b15dab9087e125140
Arhua <a0987837233@gmail.com>
2019-09-10 21:38:45 +0800

    遊戲功能完成，差join統計資料

--- BangBang/Controllers/HomeController.cs
+++ BangBang/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BangBang.Models;
+using BangBang.Models.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,42 @@ namespace BangBang.Controllers
 {
     public class HomeController : Controller
     {
+        PlayerRepository PlayerRepos = new PlayerRepository();
+
+        WalletrecordsRepository WalletrecordsRepos = new WalletrecordsRepository();
+
+        GameRecordRepository GameRecordRepos = new GameRecordRepository();
         public ActionResult Index()
         {
+
             return View();
         }
+
+        [HttpPost]
+        public ActionResult StoretoDB(String playername, int previousAmount, int latestAmount)
+        {
+
+            List<Player> QueryResult = PlayerRepos.GetByPlayerName(playername);
+            if (QueryResult.Count > 0)
+            {
+                string result = QueryResult[0].PlayerName;
+                Player player = new Player { PlayerName = result };
+                long ts = long.Parse(DateTime.Now.ToString("yyyyMMddHHmmss"));
+                Walletrecord walletrecord = new Walletrecord { Timestamp = ts, PreviousAmount = previousAmount, LatestAmount = (previousAmount+latestAmount), PlayerID = QueryResult[0].PlayerID };
+                WalletrecordsRepos.Add(walletrecord);
+                GameRecord gameRecord = new GameRecord { Timestamp=ts, Stake= latestAmount, PlayerID = QueryResult[0].PlayerID };
+                GameRecordRepos.Add(gameRecord);
+                WalletrecordsRepos.Savechanges();
+                GameRecordRepos.Savechanges();
+                return Json(result);
+            }
+            else
+            {
+                return Json(false);
+            }
+        }
+
+
 
         public ActionResult About()
         {
---
+++ BangBang/Models/Repositories/GameRecordRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BangBang.Models.Repositories
+{
+    public class GameRecordRepository : Repository<GameRecord>
+    {
+    }
+}
